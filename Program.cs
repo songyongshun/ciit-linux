8920e9d90d7df4b11cd966291844f76aa93f3523
Yongshun Song <ys_song@foxmail.com>
2026-03-22 06:42:25 +0800

    init this project

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Statiq.App;
+using Statiq.Web;
+
+namespace ciit_linux
+{
+    public class Program
+    {
+        public static async Task<int> Main(string[] args) =>
+            await Bootstrapper
+                .Factory
+                .CreateWeb(args)
+                .RunAsync();
+    }
+}
