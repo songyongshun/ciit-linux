6ee1655ebdcef343c8d24e88fafb9ccf06683fb6
Yongshun Song <ys.song320@gmail.com>
2026-04-13 20:48:03 +0800

    add gitignore

--- obj/Debug/net8.0/ciit-linux.AssemblyInfo.cs
+++ obj/Debug/net8.0/ciit-linux.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ciit-linux")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13e65f1b3920a794a6821596763f750018a34c97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+283e7b2b97ba1a565ed97c7546886665d5f0da9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ciit-linux")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ciit-linux")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
